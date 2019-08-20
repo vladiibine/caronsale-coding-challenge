6696c67249e6d46591dd37804c1c26bb2bc9ac8f
Gunther Schulz <jobs@guntherschulz.de>
2019-08-20 21:54:36 +0200

    delete obsolete files

--- challenges/web-frontend/caronsale/libs/data/src/index.ts
+++ challenges/web-frontend/caronsale/libs/data/src/index.ts
@@ -1,4 +1,1 @@
-export * from './lib/+state/users.facade';
-export * from './lib/+state/users.reducer';
-export * from './lib/+state/users.selectors';
 export * from './lib/data.module';
--- challenges/web-frontend/caronsale/libs/data/src/lib/data.module.ts
+++ challenges/web-frontend/caronsale/libs/data/src/lib/data.module.ts
@@ -1,17 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import * as fromUsers from './+state/users.reducer';
-import { UsersEffects } from './+state/users.effects';
-import { UsersFacade } from './+state/users.facade';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    StoreModule.forFeature(fromUsers.USERS_FEATURE_KEY, fromUsers.reducer),
-    EffectsModule.forFeature([UsersEffects])
-  ],
-  providers: [UsersFacade]
+  imports: [CommonModule]
 })
 export class DataModule {}
