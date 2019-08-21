405f13e26031f95088cd77e4bb19a9c2540203e3
Gunther Schulz <jobs@guntherschulz.de>
2019-08-21 20:17:58 +0200

    add action grid

--- challenges/web-frontend/caronsale/libs/ui/src/lib/auction-card/auction-card.component.ts
+++ challenges/web-frontend/caronsale/libs/ui/src/lib/auction-card/auction-card.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
 import { SalesmanAuctionsView } from '@caronsale/auctions';
+import { Observable, of } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { IMAGE_PLACEHOLDER_URL } from '../common';
 
 @Component({
   selector: 'caronsale-auction-card',
@@ -7,8 +11,28 @@ import { SalesmanAuctionsView } from '@caronsale/auctions';
   styleUrls: ['./auction-card.component.styl']
 })
 export class AuctionCardComponent implements OnInit {
-  @Input() auctions!: SalesmanAuctionsView[];
-  constructor() {}
+  imagePlaceholder = IMAGE_PLACEHOLDER_URL;
 
-  ngOnInit() {}
+  @Input() auctions!: SalesmanAuctionsView[];
+  public cols: Observable<number | undefined> = of(1);
+  public gridColsStart = 0;
+
+  constructor(private mediaObserver: MediaObserver) {}
+
+  ngOnInit() {
+    // https://brianflove.com/2017/05/03/responsive-angular/
+    const grid = new Map([
+      ['xs', 1],
+      ['sm', 2],
+      ['md', 3],
+      ['lg', 4],
+      ['xl', 5]
+    ]);
+    this.cols = this.mediaObserver.asObservable().pipe(
+      map(change => {
+        return grid.get(change[0].mqAlias);
+      }),
+      startWith(this.gridColsStart)
+    );
+  }
 }
