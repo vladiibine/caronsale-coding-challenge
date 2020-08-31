bbb93339ec0f8f350be108cf90e287f9bb5fda47
Vlad Ardelean <vlad.george.ardelean@gmail.com>
2020-09-01 00:43:59 +0300

    AuctionMonitor and CarOnSaleClient classes work; todo - tests and refactoring

--- challenges/backend/src/app/AuctionMonitorApp.ts
+++ challenges/backend/src/app/AuctionMonitorApp.ts
@@ -1,20 +1,58 @@
 import {inject, injectable} from "inversify";
+import "reflect-metadata";
+
 import {ILogger} from "./services/Logger/interface/ILogger";
 import {DependencyIdentifier} from "./DependencyIdentifiers";
-import "reflect-metadata";
+import {IApiAuction, IApiResult, ICarOnSaleClient} from "./services/CarOnSaleClient/interface/ICarOnSaleClient";
 
 @injectable()
 export class AuctionMonitorApp {
-
-    public constructor(@inject(DependencyIdentifier.LOGGER) private logger: ILogger) {
+    public constructor(
+        @inject(DependencyIdentifier.LOGGER) private logger: ILogger,
+        @inject(DependencyIdentifier.CAR_ON_SALE_CLIENT) private carOnSaleClient: ICarOnSaleClient,
+    ) {
     }
 
     public async start(): Promise<void> {
 
         this.logger.log(`Auction Monitor started.`);
 
-        // TODO: Retrieve auctions and display aggregated information (see README.md)
+        let auctions = await this.carOnSaleClient.getRunningAuctions();
 
+        if (auctions.error) {
+            this.logger.log(`For some reason, can't get the auctions! Aborting!`);
+            process.exit(-1);  //and of course, by -1, we mean 255
+
+        } else {
+            // VladA - for simplicity's sake, I assume that auction.data.total refers to the number of items
+            this.logger.log(`The number of auctions might be ${auctions.data.total}`)
+
+            // VladA: TODO - not handling pagination atm, as I don't know how it works
+            this.logger.log(`The average number of bids: ${AuctionMonitorApp.getAverageNumberOfBids(auctions.data.items)}`)
+
+            // VladA: TODO - not handling pagination here, as I don't know how it works
+            this.logger.log(`The average percentage of auction progress: ${AuctionMonitorApp.getAveragePercentageAuctionProgress(auctions.data.items)}`)
+        }
     }
 
+    // TODO - move this to an injectable class. Probably testing would be nicer like that
+    private static getAverageNumberOfBids(auctions: [IApiAuction]): number {
+        let bids = 0;
+
+        for (let a of auctions) {
+            bids += a.numBids;
+        }
+
+        return auctions.length ? bids / auctions.length : 0;
+    }
+
+    // TODO - move this to an injectable class. Probably testing would be nicer like that
+    private static getAveragePercentageAuctionProgress(items: [IApiAuction]): number {
+        let auctionProgress = 0;
+        for (let auction of items) {
+            auctionProgress += auction.currentHighestBidValue / auction.minimumRequiredAsk * 100;
+        }
+
+        return auctionProgress / items.length;
+    }
 }
--- challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
@@ -1,8 +1,87 @@
-import {ICarOnSaleClient} from "../interface/ICarOnSaleClient";
+import * as crypto from 'crypto';
+import {injectable, inject} from "inversify";
+import {default as axios} from 'axios';
 
+import {ICarOnSaleClient, ERRORS, IApiResult} from "../interface/ICarOnSaleClient";
+import {DependencyIdentifier} from "../../../DependencyIdentifiers";
+
+export const BASE_URL = `https://caronsale-backend-service-dev.herokuapp.com`;
+
+@injectable()
 export class CarOnSaleClient implements ICarOnSaleClient {
-    getRunningAuctions(): Promise<any> {
-        return Promise.resolve(undefined);
+    private userMailId: string = 'salesman@random.com';
+    // private userMailId: string = 'salesman';
+    private password: string = '123test';
+
+    private authToken: string = null;
+    private userId: string = null;
+
+    public constructor(
+        @inject(DependencyIdentifier.LOGGER) private logger
+    ) {
     }
 
+    async getRunningAuctions(): Promise<IApiResult> {
+        if (this.authToken == null) {
+            try {
+                await this.authenticate();
+
+            } catch (e) {
+                return {
+                    error: ERRORS.COULD_NOT_AUTHENTICATE,
+                    data: null,
+                }
+            }
+        }
+        // The result contains a "userId" and a "token" field that have to be set as "userid" / "authtoken"
+        // HTTP header in further requests.
+        return new Promise((resolve, reject) => {
+            axios.get(`${BASE_URL}/api/v2/auction/buyer/`,
+                {
+                    headers: {
+                        'userid': this.userId,
+                        'authtoken': this.authToken,
+                    }
+                }
+            )
+            .then(response => {
+                resolve({error: null, data: response.data})
+            })
+                .catch(reason => {this.logger.log(`Couldn't get the auction information: ${reason}`)})
+        });
+    }
+
+    private static hashPassword(password: string): string {
+        let hash = `${password}`;
+
+        for (let i = 0; i < 5; i++) {
+            hash = crypto.createHash('sha512').update(hash).digest('hex');
+        }
+
+        return hash;
+    }
+
+    private async authenticate() {
+        let path = `v2/auction/buyer`;
+
+        let endpoint = `${BASE_URL}/${path}`;
+        let authEndpoint = `${BASE_URL}/api/v1/authentication/${this.userMailId}`;
+
+        let authResult = await axios.put(
+            `${authEndpoint}`,
+            {
+                password: CarOnSaleClient.hashPassword(this.password),
+            },
+            {
+                headers: {
+                    'content-type': 'application/json'
+                }
+            }
+        );
+
+        this.logger.log('Successfully authenticated!');
+
+        this.authToken = authResult.data.token;
+        this.userId = authResult.data.userId;
+    }
 }
