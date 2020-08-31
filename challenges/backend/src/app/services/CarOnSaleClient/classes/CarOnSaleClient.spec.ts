e76cefb94d58ec10611f704df779550c81d7b36a
Vlad Ardelean <vlad.george.ardelean@gmail.com>
2020-08-31 11:59:00 +0300

    Added a dummy test which works

---
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.spec.ts
@@ -0,0 +1,12 @@
+import 'mocha';
+const expect = require('chai').expect;
+
+describe('Hello function', () => {
+
+    it('test that the testing framework works', () => {
+        // const result = hello();
+        // @ts-ignore
+        expect(1).to.equal(1);
+    });
+
+});
