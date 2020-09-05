d47fc46e349b065a7bc33d5ace6d793bf00d545f
Vlad Ardelean <vlad.george.ardelean@gmail.com>
2020-09-05 20:20:43 +0300

    Fixed IStatsCalculator.ts interface definitions

--- challenges/backend/src/app/services/StatsCalculator/interface/IStatsCalculator.ts
+++ challenges/backend/src/app/services/StatsCalculator/interface/IStatsCalculator.ts
@@ -1,7 +1,7 @@
 import {IApiAuction} from "../../CarOnSaleClient/interface/ICarOnSaleClient";
 
 export interface IStatsCalculator {
-    getAverageNumberOfBids(auctions: [IApiAuction]): number;
+    getAverageNumberOfBids(auctions: IApiAuction[]): number;
 
-    getAveragePercentageAuctionProgress(items: [IApiAuction]): number;
+    getAveragePercentageAuctionProgress(items: IApiAuction[]): number;
 }
