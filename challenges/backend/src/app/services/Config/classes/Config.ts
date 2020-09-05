d35028f4b5cb16f74d812b061fd500c5f1736955
Vlad Ardelean <vlad.george.ardelean@gmail.com>
2020-09-05 03:54:47 +0300

    Refactored Config.ts - it was too long

--- challenges/backend/src/app/services/Config/classes/Config.ts
+++ challenges/backend/src/app/services/Config/classes/Config.ts
@@ -2,30 +2,30 @@ import {injectable} from "inversify";
 
 import {ConfigOption, IConfig} from "../interface/IConfig";
 
+const CONFIGS = {
+    [ConfigOption.USER_EMAIL_ID]: 'salesman@random.com',
+    [ConfigOption.PASSWORD]: '123test',
+    [ConfigOption.API_BASE_URL]: `https://caronsale-backend-service-dev.herokuapp.com`,
+    [ConfigOption.API_BUYER_AUCTIONS]: `api/v2/auction/buyer/`,
+    [ConfigOption.API_AUTHENTICATION_ENDPOINT]: `api/v1/authentication`,
+}
+
+/**
+ * This class is mostly useful so that tests are easier to write
+ * ...because of course the CONFIGS constant could just as well be used.
+ * However, configurations are not trivial things, so better to have a
+ * class which manages them.
+ *
+ */
 @injectable()
 export class Config implements IConfig {
     getOption(option: ConfigOption): string {
-        // TODO - this feels a little over-engineered
-        //  Did it so I could mock the getOption method though
-        //  and because I expect an actual config object to have
-        //  methods that read environment variables.
-        //  For this project, a JS object could have been enough though
+        let result = CONFIGS[option];
 
-        if (option === ConfigOption.USER_EMAIL_ID) {
-            return 'salesman@random.com'
-
-        } else if (option === ConfigOption.PASSWORD) {
-            return '123test';
-
-        } else if (option === ConfigOption.API_BASE_URL) {
-            return `https://caronsale-backend-service-dev.herokuapp.com`;
-
-        } else if (option === ConfigOption.API_BUYER_AUCTIONS){
-            return `api/v2/auction/buyer/`;
-
-        } else if (option === ConfigOption.API_AUTHENTICATION_ENDPOINT){
-            return `api/v1/authentication`;
+        if (result === undefined){
+            throw new Error("Provided option doesn't exist!")
+        } else {
+            return result;
         }
-        throw new Error("Provided option doesn't exist!")
     }
 }
